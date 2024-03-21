5405da76473613613309310ff80fcc5740bf1da1
Norman Zhi <normanzyb@gmail.com>
2024-03-21 01:18:18 +0000

    ingenic_emc_nand: Initial NAND read support

--- include/hw/block/ingenic_emc.h
+++ include/hw/block/ingenic_emc.h
@@ -23,11 +23,16 @@
 
 #include "hw/sysbus.h"
 #include "qom/object.h"
+#include "hw/block/block.h"
+
+// EMC
 
 #define TYPE_INGENIC_EMC "ingenic-emc"
 OBJECT_DECLARE_TYPE(IngenicEmc, IngenicEmcClass, INGENIC_EMC)
 
 extern MemoryRegionOps nand_io_ops;
+
+typedef struct IngenicEmcNand IngenicEmcNand;
 
 typedef struct NandOpData {
     IngenicEmc *emc;
@@ -44,6 +49,8 @@ typedef struct IngenicEmc {
     MemoryRegion nand_io_mr[4];
 
     NandOpData nand_io_data[4];
+
+    IngenicEmcNand *nand[4];
 
     // GPIO
     qemu_irq io_nand_rb;
@@ -63,5 +70,35 @@ typedef struct IngenicEmcClass
     SysBusDeviceClass parent_class;
     ResettablePhases parent_phases;
 } IngenicEmcClass;
+
+// NAND
+
+#define TYPE_INGENIC_EMC_NAND "ingenic-emc-nand"
+OBJECT_DECLARE_TYPE(IngenicEmcNand, IngenicEmcNandClass, INGENIC_EMC_NAND)
+
+typedef struct IngenicEmcNand {
+    // Class inheritance
+    DeviceState parent_obj;
+    // Properties
+    BlockBackend *blk;
+    char *nand_id_str;
+    uint64_t nand_id;
+    uint32_t total_pages;
+    uint32_t page_size;
+    uint32_t oob_size;
+    uint32_t cs;
+    // States
+    uint8_t prev_cmd;
+    uint32_t addr_ofs;
+    uint64_t addr;
+    // Read/write buffers
+    uint8_t *page_buf;
+    uint32_t page_ofs;
+} IngenicEmcNand;
+
+typedef struct IngenicEmcNandClass
+{
+    SysBusDeviceClass parent_class;
+} IngenicEmcNandClass;
 
 #endif /* INGENIC_EMC_H */
