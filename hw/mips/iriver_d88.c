779512d9e1ea59b1069a12c5a55bb95991f68284
Norman Zhi <normanzyb@gmail.com>
2024-03-22 23:16:38 +0000

    ingenic_jz4755: Move SoC init to separate file

--- hw/mips/iriver_d88.c
+++ hw/mips/iriver_d88.c
@@ -32,26 +32,18 @@
 #include "hw/qdev-clock.h"
 #include "hw/mips/mips.h"
 #include "hw/char/serial.h"
-#include "hw/isa/isa.h"
-#include "net/net.h"
 #include "sysemu/sysemu.h"
 #include "hw/boards.h"
 #include "hw/mips/bios.h"
 #include "hw/loader.h"
 #include "hw/irq.h"
-#include "elf.h"
 #include "hw/sysbus.h"
 #include "hw/qdev-properties.h"
 #include "qemu/error-report.h"
 #include "sysemu/qtest.h"
 #include "sysemu/reset.h"
 #include "qemu/log.h"
-
-#include "hw/misc/ingenic_cgu.h"
-#include "hw/block/ingenic_emc.h"
-#include "hw/block/ingenic_bch.h"
-#include "hw/gpio/ingenic_gpio.h"
-#include "hw/char/ingenic_uart.h"
+#include "hw/mips/ingenic_jz4755.h"
 
 typedef struct ResetData {
     MIPSCPU *cpu;
@@ -65,100 +57,6 @@ static void main_cpu_reset(void *opaque)
 
     cpu_reset(CPU(s->cpu));
     env->active_tc.PC = s->vector & ~(target_ulong)1;
-}
-
-static MIPSCPU *jz4755_init(MachineState *machine)
-{
-    MIPSCPU *cpu;
-    CPUMIPSState *env;
-
-    /* Needs to have clocks first */
-    IngenicCgu *cgu = INGENIC_CGU(qdev_new(TYPE_INGENIC_CGU));
-    sysbus_realize_and_unref(SYS_BUS_DEVICE(cgu), &error_fatal);
-
-    /* Init CPUs. */
-    printf("%s cpu clock\n", __func__);
-    // machine->cpu_type = "XBurstR1-mips-cpu";
-    cpu = mips_cpu_create_with_clock(machine->cpu_type, qdev_get_clock_out(DEVICE(cgu), "clk-cclk"));
-    env = &cpu->env;
-
-    // Internal SRAM, 16kB
-    MemoryRegion *sys_mem = get_system_memory();
-    MemoryRegion *sram = g_new(MemoryRegion, 1);
-    memory_region_init_ram(sram, NULL, "sram", 16 * 1024, &error_fatal);
-    memory_region_add_subregion(sys_mem, 0x80000000, sram);
-
-    // TCSM SRAM, 16kB
-    MemoryRegion *tcsm = g_new(MemoryRegion, 1);
-    memory_region_init_ram(tcsm, NULL, "tcsm", 16 * 1024, &error_fatal);
-    memory_region_add_subregion(sys_mem, 0xf4000000, tcsm);
-
-    MemoryRegion *ahb0 = g_new(MemoryRegion, 1);
-    MemoryRegion *ahb1 = g_new(MemoryRegion, 1);
-    MemoryRegion *apb  = g_new(MemoryRegion, 1);
-
-    /* Register AHB0 IO space at 0x13000000. */
-    memory_region_init(ahb0, NULL, "ahb0", 0x00090000);
-    memory_region_add_subregion(sys_mem, 0x13000000, ahb0);
-
-    // Register EMC on AHB0 bus
-    IngenicEmc *emc = INGENIC_EMC(qdev_new(TYPE_INGENIC_EMC));
-    sysbus_realize_and_unref(SYS_BUS_DEVICE(emc), &error_fatal);
-    MemoryRegion *emc_mr = sysbus_mmio_get_region(SYS_BUS_DEVICE(emc), 0);
-    memory_region_add_subregion(ahb0, 0x00010000, emc_mr);
-
-    /* Register AHB1 IO space at 0x13090000. */
-    memory_region_init(ahb1, NULL, "ahb1", 0x00070000);
-    memory_region_add_subregion(sys_mem, 0x13090000, ahb1);
-
-    // Register BCH on AHB1 bus
-    IngenicBch *bch = INGENIC_BCH(qdev_new(TYPE_INGENIC_BCH));
-    sysbus_realize_and_unref(SYS_BUS_DEVICE(bch), &error_fatal);
-    MemoryRegion *bch_mr = sysbus_mmio_get_region(SYS_BUS_DEVICE(bch), 0);
-    memory_region_add_subregion(ahb1, 0x00040000, bch_mr);
-
-    /* Register APB IO space at 0x10000000. */
-    memory_region_init(apb, NULL, "apb", 0x01000000);
-    memory_region_add_subregion(sys_mem, 0x10000000, apb);
-
-    /* Register CGU on APB bus */
-    MemoryRegion *cgu_mr = sysbus_mmio_get_region(SYS_BUS_DEVICE(cgu), 0);
-    memory_region_add_subregion(apb, 0, cgu_mr);
-
-    // Register GPIOs on APB bus
-    IngenicGpio *gpio[6];
-    for (int i = 0; i < 6; i++) {
-        char name[] = "PA";
-        name[1] = 'A' + i;
-        gpio[i] = INGENIC_GPIO(qdev_new(TYPE_INGENIC_GPIO));
-        object_property_set_str(OBJECT(gpio[i]), "name", name, &error_fatal);
-        sysbus_realize_and_unref(SYS_BUS_DEVICE(gpio[i]), &error_fatal);
-        MemoryRegion *gpio_mr = sysbus_mmio_get_region(SYS_BUS_DEVICE(gpio[i]), 0);
-        memory_region_add_subregion(apb, 0x00010000 + i * 0x0100, gpio_mr);
-    }
-
-    /* Initialise 16550 UART0 at APB 0x00030000 interrupt ? */
-    ingenic_uart_init(apb, 0x00030000, env->irq[4],
-        115200, serial_hd(0), DEVICE_NATIVE_ENDIAN);
-
-    /* Initialise 16550 UART1 at APB 0x00031000 interrupt ? */
-    ingenic_uart_init(apb, 0x00031000, env->irq[5],
-        115200, serial_hd(1), DEVICE_NATIVE_ENDIAN);
-
-    /* Initialise 16550 UART2 at APB 0x00032000 interrupt ? */
-    ingenic_uart_init(apb, 0x00032000, env->irq[6],
-        115200, serial_hd(2), DEVICE_NATIVE_ENDIAN);
-
-    /* Init CPU internal devices. */
-    cpu_mips_irq_init_cpu(cpu);
-    cpu_mips_clock_init(cpu);
-
-    // Connect GPIOs
-    // PC27: NAND RB
-    qemu_irq nand_rb = qdev_get_gpio_in_named(DEVICE(gpio[2]), "in", 27);
-    qdev_connect_gpio_out_named(DEVICE(emc), "nand-rb", 0, nand_rb);
-
-    return cpu;
 }
 
 static void mips_iriver_d88_init(MachineState *machine)
@@ -175,7 +73,7 @@ static void mips_iriver_d88_init(MachineState *machine)
     //machine->ram_size = 64 * 1024 * 1024;
 
     /* Init CPUs. */
-    cpu = jz4755_init(machine);
+    cpu = ingenic_jz4755_init(machine);
     env = &cpu->env;
 
     reset_info = g_new0(ResetData, 1);
