ee80871cc4aec6a921a48cd4d7f1b7387cf923e4
Norman Zhi <normanzyb@gmail.com>
2024-06-20 01:46:31 +0100

    ar1010: Initial skeleton implementation for AR1010 radio

---
+++ include/hw/audio/ar1010.h
@@ -0,0 +1,28 @@
+/*
+ * Airoha Technology Corp. AR1000/AR1010 FM radio
+ *
+ * Copyright (c) 2024 Norman Zhi (normanzyb@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 or
+ * (at your option) version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HW_AUDIO_AR1010_H
+#define HW_AUDIO_AR1010_H
+
+#define TYPE_AR1010 "ar1010"
+
+// AR1010 I2C device address is not configurable
+#define AR1010_I2C_ADDR 0x10
+
+#endif // HW_AUDIO_AR1010_H
