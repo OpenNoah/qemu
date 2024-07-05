b02b4fcd8a2b2136c42793b3877e6dc2d6f1d54c
Norman Zhi <normanzyb@gmail.com>
2024-07-05 23:54:28 +0100

    ingenic_msc: Add DMA and interrupt requests for read

--- hw/sd/ingenic_msc.c
+++ hw/sd/ingenic_msc.c
@@ -26,8 +26,12 @@
 #include "qemu/log.h"
 #include "qemu/module.h"
 #include "migration/vmstate.h"
+#include "hw/qdev-properties.h"
 #include "hw/ssi/ingenic_msc.h"
 #include "trace.h"
+
+// 16-entry x 32-bit data FIFO
+#define DATA_FIFO_SIZE  64
 
 #define REG_CTRL    0x00
 #define REG_STAT    0x04
@@ -62,11 +66,44 @@ static void ingenic_msc_reset(Object *obj, ResetType type)
     s->reg.clkrt = 0;
     s->reg.cmdat = 0;
     s->reg.imask = 0xffff;
-    s->reg.ireg  = 0; // 0x2000;
+    if (s->model == 0x47ff)
+        s->reg.ireg  = 0x2000;
+    else
+        s->reg.ireg  = 0;
     s->reg.cmd   = 0;
     s->reg.arg   = 0;
     s->reg.lpm   = 0;
     qemu_set_irq(s->gpio_cd, sdbus_get_inserted(&s->sdbus));
+}
+
+static void ingenic_msc_update_irq(IngenicMsc *s)
+{
+    uint16_t irq = ~s->reg.imask & s->reg.ireg;
+    if (!!irq != !!s->prev_irq) {
+        s->prev_irq = irq;
+        trace_ingenic_msc_irq(irq);
+        qemu_set_irq(s->irq, !!irq);
+    }
+}
+
+static void ingenic_msc_update_rx_flags(IngenicMsc *s)
+{
+    // Update data transfer interrupt flags
+    uint32_t txfr_cnt = MIN(s->data_size, s->data_offset + DATA_FIFO_SIZE);
+    if (s->data_size == txfr_cnt) {
+        // All data has been transferred into FIFO, read complete
+        s->reg.stat |= BIT(12);
+        s->reg.ireg |= BIT(0);
+    }
+    if (s->data_offset == s->data_size) {
+        // FIFO empty
+        s->reg.stat |= BIT(6);
+    } else {
+        // FIFO not empty
+        s->reg.stat &= ~BIT(6);
+    }
+    s->reg.snob = s->data_offset / s->reg.blklen;
+    ingenic_msc_update_irq(s);
 }
 
 static void ingenic_msc_start(IngenicMsc *s)
@@ -149,7 +186,7 @@ static void ingenic_msc_start(IngenicMsc *s)
 
     // Process data transfer
     if (s->reg.cmdat & BIT(11)) {
-        qemu_log_mask(LOG_UNIMP, "%s: TODO\n", __func__);
+        qemu_log_mask(LOG_UNIMP, "%s: TODO IO_ABORT\n", __func__);
         qmp_stop(NULL);
     }
     if (s->reg.cmdat & BIT(3)) {
@@ -157,24 +194,52 @@ static void ingenic_msc_start(IngenicMsc *s)
         s->data_size = s->reg.blklen * s->reg.nob;
         if (s->reg.cmdat & BIT(4)) {
             // Write operation
-            qemu_log_mask(LOG_UNIMP, "%s: TODO\n", __func__);
+            qemu_log_mask(LOG_UNIMP, "%s: TODO WRITE\n", __func__);
             qmp_stop(NULL);
         } else {
             // Read operation
-            if (s->data_size != 0) {
-                // FIFO not empty
-                s->reg.stat &= ~BIT(6);
-            }
+            ingenic_msc_update_rx_flags(s);
+            if (s->data_size && (s->reg.cmdat & BIT(8)))
+                qemu_irq_raise(s->dma_rx);
         }
-        s->reg.snob = 0;
     }
 
+    // Workaround for ingenic kernel code
+    // It expects PRG_DONE after CMD12 regardless of read/write
+    if (request.cmd == 12)
+        s->reg.ireg |= BIT(1);
+
+    ingenic_msc_update_irq(s);
     return;
 
 err:
     // No response, timed out
+    qemu_log_mask(LOG_GUEST_ERROR, "%s: CMD%u(0x%x) failed, expecting R%u\n",
+        __func__, request.cmd, request.arg, rtype);
     s->reg.stat = (s->reg.stat & stat_mask) | (BIT(11) | BIT(1));
     s->reg.ireg |= BIT(2);
+    ingenic_msc_update_irq(s);
+}
+
+uint32_t ingenic_msc_dma_rx(IngenicMsc *s, uint8_t *buf, uint32_t len)
+{
+    len = MIN(len, s->data_size - s->data_offset);
+    trace_ingenic_msc_dma_rx(len);
+    for (uint32_t ofs = 0; ofs < len; ofs += 4) {
+        if (s->data_offset % ARRAY_SIZE(s->data_fifo) == 0) {
+            // FIFO is empty, read more data
+            uint32_t rlen = MIN(ARRAY_SIZE(s->data_fifo), s->data_size - s->data_offset);
+            trace_ingenic_msc_fifo_offset(s->data_offset, rlen);
+            sdbus_read_data(&s->sdbus, &s->data_fifo[0], rlen);
+        }
+
+        // From host mem to host mem, endianness doesn't matter
+        uint32_t v = ldl_he_p(&s->data_fifo[s->data_offset % ARRAY_SIZE(s->data_fifo)]);
+        stl_he_p(&buf[ofs], v);
+        s->data_offset += 4;
+    }
+    ingenic_msc_update_rx_flags(s);
+    return len;
 }
 
 static uint64_t ingenic_msc_read(void *opaque, hwaddr addr, unsigned size)
@@ -210,25 +275,20 @@ static uint64_t ingenic_msc_read(void *opaque, hwaddr addr, unsigned size)
     case REG_RXFIFO:
         if (s->data_offset != s->data_size) {
             if (s->data_offset % ARRAY_SIZE(s->data_fifo) == 0) {
-                // Read more data
+                // FIFO is empty, read more data
                 uint32_t rlen = MIN(ARRAY_SIZE(s->data_fifo), s->data_size - s->data_offset);
                 trace_ingenic_msc_fifo_offset(s->data_offset, rlen);
                 sdbus_read_data(&s->sdbus, &s->data_fifo[0], rlen);
             }
         }
         data = ldl_le_p(&s->data_fifo[s->data_offset % ARRAY_SIZE(s->data_fifo)]);
-        if (s->data_offset != s->data_size)
+        if (likely(s->data_offset != s->data_size)) {
             s->data_offset += 4;
-        if (s->data_offset == s->data_size) {
-            // Read complete
-            // FIFO empty, data transfer done
-            s->reg.stat |= BIT(12) | BIT(6);
-            s->reg.ireg |= BIT(0);
         } else {
-            // FIFO not empty
-            s->reg.stat &= ~BIT(6);
+            qemu_log_mask(LOG_GUEST_ERROR, "%s: Read beyond available data\n", __func__);
+            qmp_stop(NULL);
         }
-        s->reg.snob = s->data_offset / s->reg.blklen;
+        ingenic_msc_update_rx_flags(s);
         break;
     case REG_LPM:
         data = s->reg.lpm;
@@ -279,9 +339,11 @@ static void ingenic_msc_write(void *opaque, hwaddr addr, uint64_t data, unsigned
         break;
     case REG_IMASK:
         s->reg.imask = data | 0x0018;
+        ingenic_msc_update_irq(s);
         break;
     case REG_IREG:
         s->reg.ireg = s->reg.ireg & ~(data & 7);
+        ingenic_msc_update_irq(s);
         break;
     case REG_CMD:
         s->reg.cmd = data & 0x3f;
@@ -311,16 +373,26 @@ static void ingenic_msc_init(Object *obj)
     memory_region_init_io(&s->mr, OBJECT(s), &msc_ops, s, "msc", 0x1000);
     sysbus_init_mmio(SYS_BUS_DEVICE(obj), &s->mr);
     qbus_init(&s->sdbus, sizeof(s->sdbus), TYPE_INGENIC_SDHOST_BUS, DEVICE(s), "sd-bus");
-    qdev_init_gpio_out_named(DEVICE(obj), &s->irq, "irq-out", 1);
     qdev_init_gpio_out_named(DEVICE(obj), &s->gpio_cd, "io-cd", 1);
+    qdev_init_gpio_out_named(DEVICE(obj), &s->dma_tx, "dma-tx-req", 1);
+    qdev_init_gpio_out_named(DEVICE(obj), &s->dma_rx, "dma-rx-req", 1);
+    qdev_init_gpio_out_named(DEVICE(obj), &s->irq, "irq-out", 1);
 }
 
 static void ingenic_msc_finalize(Object *obj)
 {
 }
 
+static Property ingenic_msc_properties[] = {
+    DEFINE_PROP_UINT32("model", IngenicMsc, model, 0x4755),
+    DEFINE_PROP_END_OF_LIST(),
+};
+
 static void ingenic_msc_class_init(ObjectClass *class, void *data)
 {
+    DeviceClass *dc = DEVICE_CLASS(class);
+    device_class_set_props(dc, ingenic_msc_properties);
+
     IngenicMscClass *msc_class = INGENIC_MSC_CLASS(class);
     ResettableClass *rc = RESETTABLE_CLASS(class);
     resettable_class_set_parent_phases(rc,
