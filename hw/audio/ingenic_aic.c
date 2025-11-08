577e68e1cae71ce737d858b3c16df7e955c39203
Norman Zhi <normanzyb@gmail.com>
2025-11-08 01:57:28 +0000

    ingenic_aic: Initial support for audio playback

--- hw/audio/ingenic_aic.c
+++ hw/audio/ingenic_aic.c
@@ -30,27 +30,29 @@
 #include "hw/audio/ingenic_aic.h"
 #include "trace.h"
 
-#define REG_AICFR   0x00
-#define REG_AICCR   0x04
-#define REG_ACCR1   0x08
-#define REG_ACCR2   0x0c
-#define REG_I2SCR   0x10
-#define REG_AICSR   0x14
-#define REG_ACSR    0x18
-#define REG_I2SSR   0x1c
-#define REG_ACCAR   0x20
-#define REG_ACCDR   0x24
-#define REG_ACSAR   0x28
-#define REG_ACSDR   0x2c
-#define REG_I2SDIV  0x30
-#define REG_AICDR   0x34
-// JZ4740
-#define REG_CDCCR1  0x80
-#define REG_CDCCR2  0x84
-// JZ4755
-#define REG_CKCFG   0xa0
-#define REG_RGADW   0xa4
-#define REG_RGDATA  0xa8
+typedef enum IngenicAicReg {
+    REG_AICFR  = 0x00,
+    REG_AICCR  = 0x04,
+    REG_ACCR1  = 0x08,
+    REG_ACCR2  = 0x0c,
+    REG_I2SCR  = 0x10,
+    REG_AICSR  = 0x14,
+    REG_ACSR   = 0x18,
+    REG_I2SSR  = 0x1c,
+    REG_ACCAR  = 0x20,
+    REG_ACCDR  = 0x24,
+    REG_ACSAR  = 0x28,
+    REG_ACSDR  = 0x2c,
+    REG_I2SDIV = 0x30,
+    REG_AICDR  = 0x34,
+    // JZ4740
+    REG_CDCCR1 = 0x80,
+    REG_CDCCR2 = 0x84,
+    // JZ4755
+    REG_CKCFG  = 0xa0,
+    REG_RGADW  = 0xa4,
+    REG_RGDATA = 0xa8,
+} IngenicAicReg;
 
 void qmp_stop(Error **errp);
 
@@ -66,6 +68,115 @@ static void ingenic_aic_reset(Object *obj, ResetType type)
     s->reg.aicsr = BIT(3);
     s->reg.cdccr1 = 0x001b2302;
     s->reg.cdccr2 = 0x00170803;
+
+    AUD_set_active_out(s->voice.out, false);
+
+    s->srate = 0;
+    s->out.bitw = 0;
+    s->out.fifo_rptr = 0;
+    s->out.fifo_wptr = 0;
+}
+
+uint32_t ingenic_aic_dma_tx_available(IngenicAic *s)
+{
+    const uint32_t fifo_size = ARRAY_SIZE(s->out.fifo);
+    return (fifo_size - 1 - s->out.fifo_wptr + s->out.fifo_rptr) % fifo_size;
+}
+
+static void ingenic_aic_play_sample(IngenicAic *s, uint32_t v)
+{
+    int32_t smp = v << (32 - s->out.bitw);
+    // smp /= 1 << s->out.bitw;
+    s->out.fifo[s->out.fifo_wptr] = smp;
+    s->out.fifo_wptr = (s->out.fifo_wptr + 1) % ARRAY_SIZE(s->out.fifo);
+}
+
+static void ingenic_aic_pcm_out_cb(void *opaque, int available)
+{
+    IngenicAic *s = INGENIC_AIC(opaque);
+    int32_t buffer[ARRAY_SIZE(s->out.fifo)];
+    int size = 0;
+    while (s->out.fifo_rptr != s->out.fifo_wptr && size < available) {
+        buffer[size / 4] = s->out.fifo[s->out.fifo_rptr];
+        size += 4;
+        s->out.fifo_rptr = (s->out.fifo_rptr + 1) % ARRAY_SIZE(s->out.fifo);
+    }
+    AUD_write(s->voice.out, &buffer[0], size);
+    if (size) {
+        trace_ingenic_aic_pcm_sample(available, size);
+        qemu_irq_raise(s->out.dma_req);
+    }
+}
+
+static void ingenic_aic_aiccr_update(IngenicAic *s, uint32_t data)
+{
+    int out_bps = (data >> 19) & 7;
+    int in_bps  = (data >> 16) & 7;
+    static const int bps_lut[] = {8, 16, 18, 20, 24};
+    in_bps  = in_bps >= 5 ? 0 : bps_lut[in_bps];
+    out_bps = out_bps >= 5 ? 0 : bps_lut[out_bps];
+    s->out.bitw  = out_bps;
+
+    int mono = (data >> 11) & 1;
+    int play = (data >>  1) & 1;
+    int rec  = (data >>  0) & 1;
+
+    if (mono || rec || data & 0x0e00) {
+        qemu_log_mask(LOG_UNIMP, "%s: TODO 0x%x\n", __func__, data);
+        qmp_stop(NULL);
+    }
+
+    trace_ingenic_aic_aiccr(data, out_bps, in_bps, mono, play, rec);
+    s->reg.aiccr = data & 0x003fce7f;
+
+    if (data & (1 << 8)) {
+        // FIFO flush
+        AUD_set_active_out(s->voice.out, false);
+        s->out.fifo_rptr = 0;
+        s->out.fifo_wptr = 0;
+    }
+
+    if (play) {
+        if (!AUD_is_active_out(s->voice.out)) {
+            audsettings as;
+            as.nchannels = MIN(AUDIO_MAX_CHANNELS, 2);
+            as.fmt = AUDIO_FORMAT_S32;
+            as.freq = s->srate;
+            as.endianness = 0;
+            s->voice.out = AUD_open_out(&s->card, s->voice.out,
+                TYPE_INGENIC_AIC ".out", s, &ingenic_aic_pcm_out_cb, &as);
+            AUD_set_volume_out(s->voice.out, 0, 255, 255);
+            AUD_set_active_out(s->voice.out, true);
+        }
+    } else {
+        if (AUD_is_active_out(s->voice.out)) {
+            AUD_set_active_out(s->voice.out, false);
+        }
+    }
+}
+
+static void ingenic_aic_cdccr2_update(IngenicAic *s, uint32_t data)
+{
+    int srate = (data >> 8) & 0x0f;
+    if (srate >= 9) {
+        srate = 0;
+    } else {
+        static const int srate_lut[] = {8000, 11025, 12000, 16000, 22050, 24000, 32000, 44100, 48000};
+        srate = srate_lut[srate];
+    }
+    s->srate = srate;
+
+    int ain_gain_x10 = (data >> 16) & 0x1f;
+    ain_gain_x10 = ain_gain_x10 * 15 - 345;
+
+    static const int hp_gain_lut[] = {0, 2, 4, 6};
+    int hp_gain = hp_gain_lut[data & 3];
+
+    static const int mic_gain_lut[] = {0, 6, 12, 20};
+    int mic_gain = mic_gain_lut[(data >> 4) & 3];
+
+    trace_ingenic_aic_cdccr2(data, srate, hp_gain, ain_gain_x10, mic_gain);
+    s->reg.cdccr2 = data & 0x001f0f33;
 }
 
 static uint64_t ingenic_aic_read(void *opaque, hwaddr addr, unsigned size)
@@ -114,11 +225,7 @@ static void ingenic_aic_write(void *opaque, hwaddr addr, uint64_t data, unsigned
         s->reg.aicfr = data & 0xff77;
         break;
     case REG_AICCR:
-        s->reg.aiccr = data & 0x003fce7f;
-        if (data & (BIT(8) | BIT(7))) {
-            qemu_log_mask(LOG_UNIMP, "%s: TODO\n", __func__);
-            qmp_stop(NULL);
-        }
+        ingenic_aic_aiccr_update(s, data);
         break;
     case REG_I2SCR:
         s->reg.i2scr = data & 0x1011;
@@ -126,11 +233,14 @@ static void ingenic_aic_write(void *opaque, hwaddr addr, uint64_t data, unsigned
     case REG_I2SDIV:
         s->reg.i2sdiv = data & 0x0f;
         break;
+    case REG_AICDR:
+        ingenic_aic_play_sample(s, data);
+        break;
     case REG_CDCCR1:
         s->reg.cdccr1 = data & 0x3f1f7f03;
         break;
     case REG_CDCCR2:
-        s->reg.cdccr2 = data & 0x001f0f33;
+        ingenic_aic_cdccr2_update(s, data);
         break;
     default:
         qemu_log_mask(LOG_GUEST_ERROR, "%s: Unknown address " HWADDR_FMT_plx " 0x%"PRIx64"\n",
@@ -152,6 +262,16 @@ static void ingenic_aic_init(Object *obj)
     IngenicAic *s = INGENIC_AIC(obj);
     memory_region_init_io(&s->mr, OBJECT(s), &adc_ops, s, "adc", 0x1000);
     sysbus_init_mmio(SYS_BUS_DEVICE(obj), &s->mr);
+    qdev_init_gpio_out_named(DEVICE(obj), &s->out.dma_req, "dma-tx-req", 1);
+    qdev_init_gpio_out_named(DEVICE(obj), &s->in.dma_req, "dma-rx-req", 1);
+}
+
+static void ingenic_aic_realize(DeviceState *dev, Error **errp)
+{
+    IngenicAic *s = INGENIC_AIC(dev);
+    if (!AUD_register_card(TYPE_INGENIC_AIC, &s->card, errp)) {
+        return;
+    }
 }
 
 static void ingenic_aic_finalize(Object *obj)
@@ -160,6 +280,10 @@ static void ingenic_aic_finalize(Object *obj)
 
 static void ingenic_aic_class_init(ObjectClass *class, const void *data)
 {
+    DeviceClass *dc = DEVICE_CLASS(class);
+    set_bit(DEVICE_CATEGORY_SOUND, dc->categories);
+    dc->realize = &ingenic_aic_realize;
+
     IngenicAicClass *bch_class = INGENIC_AIC_CLASS(class);
     ResettableClass *rc = RESETTABLE_CLASS(class);
     resettable_class_set_parent_phases(rc,
