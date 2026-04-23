a7ba6280cf7015f2c24616681b867d3f950e73b9
Norman Zhi <normanzyb@gmail.com>
2026-04-23 23:53:27 +0100

    Initial support for IRIVER D88 with JZ4755 SoC

---
+++ hw/mips/iriver_d88.c
@@ -0,0 +1,174 @@
+/*
+ * QEMU IRIVER Dicple D88 board support
+ *
+ * Emulates a very simple machine model similar to the one used by the
+ * proprietary MIPS emulator.
+ *
+ * Copyright (c) 2024 Norman Zhi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "qemu/osdep.h"
+#include "qapi/error.h"
+#include "qemu/datadir.h"
+#include "hw/clock.h"
+#include "hw/qdev-clock.h"
+#include "hw/mips/mips.h"
+#include "hw/char/serial.h"
+#include "hw/isa/isa.h"
+#include "net/net.h"
+#include "sysemu/sysemu.h"
+#include "hw/boards.h"
+#include "hw/mips/bios.h"
+#include "hw/loader.h"
+#include "elf.h"
+#include "hw/sysbus.h"
+#include "hw/qdev-properties.h"
+#include "qemu/error-report.h"
+#include "sysemu/qtest.h"
+#include "sysemu/reset.h"
+#include "qemu/log.h"
+
+#include "hw/misc/ingenic_cgu.h"
+
+typedef struct ResetData {
+    MIPSCPU *cpu;
+    uint64_t vector;
+} ResetData;
+
+static void main_cpu_reset(void *opaque)
+{
+    ResetData *s = (ResetData *)opaque;
+    CPUMIPSState *env = &s->cpu->env;
+
+    cpu_reset(CPU(s->cpu));
+    env->active_tc.PC = s->vector & ~(target_ulong)1;
+}
+
+static MIPSCPU *jz4755_init()
+{
+    MIPSCPU *cpu;
+    CPUMIPSState *env;
+
+    /* Needs to have clocks first */
+    IngenicCgu *cgu = INGENIC_CGU(qdev_new(TYPE_INGENIC_CGU));
+    sysbus_realize_and_unref(SYS_BUS_DEVICE(cgu), &error_fatal);
+    //qdev_connect_clock_in(DEVICE(cpu), "clk-in", qdev_get_clock_out(DEVICE(cgu), "clk-cclk"));
+
+    /* Init CPUs. */
+    printf("%s cpu clock\n", __func__);
+    cpu = mips_cpu_create_with_clock("XBurstR1-mips-cpu", qdev_get_clock_out(DEVICE(cgu), "clk-cclk"));
+    env = &cpu->env;
+
+    MemoryRegion *ahb0 = g_new(MemoryRegion, 1);
+    MemoryRegion *ahb1 = g_new(MemoryRegion, 1);
+    MemoryRegion *apb  = g_new(MemoryRegion, 1);
+
+    /* Register AHB0 IO space at 0x13000000. */
+    memory_region_init_io(ahb0, NULL, NULL, NULL, "ahb0", 0x00090000);
+    memory_region_add_subregion(get_system_memory(), 0x13000000, ahb0);
+
+    /* Register AHB1 IO space at 0x13090000. */
+    memory_region_init_io(ahb1, NULL, NULL, NULL, "ahb1", 0x00070000);
+    memory_region_add_subregion(get_system_memory(), 0x13090000, ahb1);
+
+    /* Register APB IO space at 0x10000000. */
+    memory_region_init_io(apb, NULL, NULL, NULL, "apb", 0x01000000);
+    memory_region_add_subregion(get_system_memory(), 0x10000000, apb);
+
+    /* Register CGU on APB bus */
+    MemoryRegion *mr = sysbus_mmio_get_region(SYS_BUS_DEVICE(cgu), 0);
+    memory_region_add_subregion(apb, 0, mr);
+
+    /* Initialise 16550 UART0 at APB 0x00030000 interrupt ? */
+    serial_mm_init(apb, 0x00030000, 2, env->irq[4],
+        115200, serial_hd(0), DEVICE_LITTLE_ENDIAN);
+
+    /* Initialise 16550 UART1 at APB 0x00031000 interrupt ? */
+    serial_mm_init(apb, 0x00031000, 2, env->irq[5],
+        115200, serial_hd(1), DEVICE_LITTLE_ENDIAN);
+
+    /* Initialise 16550 UART2 at APB 0x00032000 interrupt ? */
+    serial_mm_init(apb, 0x00032000, 2, env->irq[6],
+        115200, serial_hd(2), DEVICE_LITTLE_ENDIAN);
+
+    /* Init CPU internal devices. */
+    cpu_mips_irq_init_cpu(cpu);
+    cpu_mips_clock_init(cpu);
+
+    return cpu;
+}
+
+static void mips_iriver_d88_init(MachineState *machine)
+{
+    char *filename;
+    MemoryRegion *address_space_mem = get_system_memory();
+    MemoryRegion *bootrom = g_new(MemoryRegion, 1);
+    MIPSCPU *cpu;
+    CPUMIPSState *env;
+    ResetData *reset_info;
+    int bootrom_size;
+
+    /* Init CPUs. */
+    cpu = jz4755_init();
+    env = &cpu->env;
+
+    reset_info = g_new0(ResetData, 1);
+    reset_info->cpu = cpu;
+    reset_info->vector = env->active_tc.PC;
+    qemu_register_reset(main_cpu_reset, reset_info);
+
+    /* Allocate RAM. */
+    memory_region_init_rom(bootrom, NULL, "mips_iriver_d88.bootrom", 8 * 1024,
+                           &error_fatal);
+
+    memory_region_add_subregion(address_space_mem, 0LL, machine->ram);
+
+    /* Map the BIOS / boot exception handler. */
+    memory_region_add_subregion(address_space_mem, 0x1fc00000LL, bootrom);
+    /* Load a BIOS / boot exception handler image. */
+    filename = qemu_find_file(QEMU_FILE_TYPE_BIOS, machine->firmware);
+    if (filename) {
+        bootrom_size = load_image_targphys(filename, 0x1fc00000LL, 8 * 1024);
+        g_free(filename);
+    } else {
+        bootrom_size = -1;
+    }
+    if ((bootrom_size < 0 || bootrom_size > BIOS_SIZE) &&
+        machine->firmware && !qtest_enabled()) {
+        /* Bail out if we have neither a kernel image nor boot vector code. */
+        error_report("Could not load MIPS bios '%s'", machine->firmware);
+        exit(1);
+    } else {
+        /* We have a boot vector start address. */
+        env->active_tc.PC = (target_long)(int32_t)0xbfc00000;
+    }
+}
+
+static void mips_iriver_d88_machine_init(MachineClass *mc)
+{
+    mc->desc = "MIPS IRIVER Dicple D88 platform";
+    mc->init = mips_iriver_d88_init;
+    mc->default_cpu_type = MIPS_CPU_TYPE_NAME("XBurstR1");
+    mc->default_ram_id = "mips_iriver_d88.ram";
+    mc->default_ram_size = 16 * 1024;
+}
+
+DEFINE_MACHINE("iriver_d88", mips_iriver_d88_machine_init)
---
+++ hw/misc/ingenic_cgu.c
@@ -0,0 +1,190 @@
+/*
+ * Ingenic JZ4755 Clock Reset and Power Controller emulation
+ *
+ * Copyright (c) 2024 Norman Zhi
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "qemu/osdep.h"
+#include "qapi/error.h"
+#include "cpu.h"
+#include "trace.h"
+#include "qemu/log.h"
+#include "qemu/module.h"
+#include "hw/sysbus.h"
+#include "hw/qdev-clock.h"
+#include "migration/vmstate.h"
+
+#include "hw/qdev-properties.h"
+#include "hw/misc/ingenic_cgu.h"
+
+void qmp_stop(Error **errp);
+
+static void ingenic_cgu_reset(Object *obj, ResetType type)
+{
+    IngenicCgu *s = INGENIC_CGU(obj);
+
+    // Initial register values
+    s->CPCCR  = 0x42040000;
+    s->CPCCR  = 0x28080011;
+    s->CPPSR  = 0x80000000;
+    s->I2SCDR = 0x00000004;
+    s->LPCDR  = 0x00000004;
+    s->MSCCDR = 0x00000000;
+    s->SSICDR = 0x00000000;
+    s->CIMCDR = 0x00000004;
+    s->LCR    = 0x000000f8;
+    s->CLKGR  = 0x00000000;
+    s->OPCR   = 0x00001500;
+    s->RSR    = 0x00000001;
+}
+
+static void ingenic_cgu_update_clocks(IngenicCgu *cgu)
+{
+    // Update clock frequencies
+    uint32_t ext_clk = 24000000;
+    uint32_t sys_clk = ext_clk;
+
+    if ((cgu->CPPCR & (BIT(8) | BIT(9))) == BIT(8)) {
+        // Switch to PLL
+        uint32_t m = (cgu->CPPCR >> 23) + 2;
+        uint32_t n = ((cgu->CPPCR >> 18) & 0x1f) + 2;
+        uint32_t od = (cgu->CPPCR >> 16) & 3;
+        static const uint32_t od_map[] = {1, 2, 2, 4};
+        od = od_map[od];
+        uint32_t pll_clk = ext_clk * m / n / od;
+        sys_clk = pll_clk;
+    }
+
+    uint32_t cclk = sys_clk;
+    uint32_t cdiv = cgu->CPCCR & 0x0f;
+    static const uint32_t cdiv_map[16] = {1, 2, 3, 4, 6, 8, 0};
+    cdiv = cdiv_map[cdiv];
+    if (unlikely(cdiv == 0)) {
+        qemu_log_mask(LOG_GUEST_ERROR, "CGU cclk div by 0\n");
+        cdiv = 1;
+    }
+    cclk /= cdiv;
+
+    qemu_log("CGU freq cclk %"PRIu32"\n", cclk);
+    clock_set_hz(qdev_get_clock_out(DEVICE(cgu), "clk-cclk"), cclk);
+}
+
+static uint64_t ingenic_cgu_read(void *opaque, hwaddr addr, unsigned size)
+{
+    if (unlikely(size != 4 || (addr & 3) != 0)) {
+        qemu_log_mask(LOG_GUEST_ERROR, "CGU read unaligned @ " HWADDR_FMT_plx "/%"PRIx32"\n",
+                      addr, (uint32_t)size);
+        qmp_stop(NULL);
+        return 0;
+    }
+
+    IngenicCgu *cgu = opaque;
+    hwaddr aligned_addr = addr; // & ~3;
+    uint64_t data = 0;
+    switch (aligned_addr) {
+    case 0x00:
+        data = cgu->CPCCR;
+        break;
+    case 0x10:
+        data = cgu->CPPCR;
+        break;
+    case 0x20:
+        data = cgu->CLKGR;
+        break;
+    default:
+        qemu_log_mask(LOG_GUEST_ERROR, "CGU read unknown address " HWADDR_FMT_plx "\n", aligned_addr);
+        qmp_stop(NULL);
+    }
+    //data = (data >> (8 * (addr & 3))) & ((1LL << (8 * size)) - 1);
+    qemu_log("CGU read @ " HWADDR_FMT_plx "/%"PRIx32": 0x%"PRIx64"\n", addr, (uint32_t)size, data);
+    return data;
+}
+
+static void ingenic_cgu_write(void *opaque, hwaddr addr, uint64_t data, unsigned size)
+{
+    if (unlikely(size != 4 || (addr & 3) != 0)) {
+        qemu_log_mask(LOG_GUEST_ERROR, "CGU write unaligned @ " HWADDR_FMT_plx "/%"PRIx32" 0x%"PRIx64"\n",
+                      addr, (uint32_t)size, data);
+        qmp_stop(NULL);
+        return;
+    }
+
+    IngenicCgu *cgu = opaque;
+    hwaddr aligned_addr = addr; // & ~3;
+    qemu_log("CGU write @ " HWADDR_FMT_plx "/%"PRIx32": 0x%"PRIx64"\n", addr, (uint32_t)size, data);
+    switch (aligned_addr) {
+    case 0x00:
+        cgu->CPCCR = data & 0xffefffff;
+        ingenic_cgu_update_clocks(cgu);
+        break;
+
+    case 0x10:
+        cgu->CPPCR = data & 0xffff03ff;
+        if (cgu->CPPCR & BIT(8)) {
+            // PLL ON
+            cgu->CPPCR |= BIT(10);
+        }
+        ingenic_cgu_update_clocks(cgu);
+        break;
+
+    case 0x20:
+        cgu->CLKGR = data & 0x01ffffff;
+        break;
+
+    default:
+        qemu_log_mask(LOG_GUEST_ERROR, "CGU write unknown address " HWADDR_FMT_plx " 0x%"PRIx64"\n", addr, data);
+        qmp_stop(NULL);
+        return;
+    }
+}
+
+static MemoryRegionOps cgu_ops = {
+    .read = ingenic_cgu_read,
+    .write = ingenic_cgu_write,
+    .endianness = DEVICE_LITTLE_ENDIAN,
+};
+
+static void ingenic_cgu_init(Object *obj)
+{
+    printf("%s enter\n", __func__);
+    SysBusDevice *sbd = SYS_BUS_DEVICE(obj);
+    IngenicCgu *s = INGENIC_CGU(obj);
+
+    memory_region_init_io(&s->mr, OBJECT(s), &cgu_ops, s, "cgu", 0x1000);
+    sysbus_init_mmio(sbd, &s->mr);
+
+    qdev_init_clock_out(DEVICE(s), "clk-cclk");
+    ingenic_cgu_update_clocks(s);
+    printf("%s end\n", __func__);
+}
+
+static void ingenic_cgu_finalize(Object *obj)
+{
+    printf("%s enter\n", __func__);
+}
+
+static void ingenic_cgu_class_init(ObjectClass *class, void *data)
+{
+    IngenicCguClass *cgu_class = INGENIC_CGU_CLASS(class);
+    ResettableClass *rc = RESETTABLE_CLASS(class);
+    resettable_class_set_parent_phases(rc,
+                                       ingenic_cgu_reset,
+                                       NULL,
+                                       NULL,
+                                       &cgu_class->parent_phases);
+}
+
+OBJECT_DEFINE_TYPE(IngenicCgu, ingenic_cgu, INGENIC_CGU, SYS_BUS_DEVICE)
